026f3e952591fa99957ab70beac3555d3b99813c
gaohan1994 <871418277@qq.com>
2021-09-28 15:36:26 +0800

    * 打开 webpack 配置 * 新增 按需加载 * 新增 splitchunks 配置

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Index from './pages';
 import reportWebVitals from './reportWebVitals';
+// import 'antd/dist/antd.css';
 
 ReactDOM.render(
   <React.StrictMode>
